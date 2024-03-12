248b204db5a30a84d52c019c63aff6032637f91a
sumit <sk03950@gmail.com>
2024-03-12 10:17:13 +0530

    form working

--- call-back.php
+++ call-back.php
@@ -1,10 +1,10 @@
 <?php 
 if(isset($_POST['submit'])){
     $email_to = "akkushwaha018@gmail.com"; // this is your Email address
-    $from = $_POST['email_id'];
-    $name = $_POST['your_name'];
-    $mobile = $_POST['mobile_number'];
-    $customer_query = $_POST['customer_query'];
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $mobile = $_POST['phone'];
+    $customer_query = $_POST['messagee'];
 
     $subject = "Book OPD at ₹ 99";
     $message = "Name:- ". $name . "\n\n" . "Email:- ". $from. "\n\n" . "Mobile Number:- " . $mobile. "\n\n". "Query:- ".$_POST['customer_query'];
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $email_to;
     mail($email_to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    header('Location: thank-you-srinagar.html');
+    // echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    header('Location: thank-you.html');
     }
 ?>
